9b3bea2a0d2a0336da3396b7b2e4000cc19eb900
Krzysztof Rutka <krzysztof.rutka@gmail.com>
2011-11-25 17:13:35 +0100

    Added ensure => present to defaults.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,25 @@
-package { 
-  "curl": 
-    ensure => present; 
+package {
+  "curl":
+    ensure => present;
 }
 
-exec { 
-  "kerl": 
-    path    => ["/bin", "/usr/bin"], 
-    cwd     => "/usr/local/bin", 
+exec {
+  "kerl":
+    path    => ["/bin", "/usr/bin"],
+    cwd     => "/usr/local/bin",
     command => "curl -O https://raw.github.com/spawngrid/kerl/master/kerl",
     creates => "/usr/local/bin/kerl",
     require => Package["curl"];
 }
 
 file {
-  "/usr/local/bin/kerl":
+  "kerl":
+    path    => "/usr/local/bin/kerl",
     mode    => 755,
     require => Exec["kerl"];
 }
 
-Erlang { 
-  require => File["kerl"] 
+Erlang {
+  ensure  => present,
+  require => File["kerl"]
 }
